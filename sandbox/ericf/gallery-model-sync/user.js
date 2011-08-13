327d55fad626f1ea8a5178a0c063f1debf88fe0f
Eric Ferraiuolo <eferraiuolo@gmail.com>
2011-08-13 15:43:10 -0400

    OMG, awesome URL handling with convention over configuration!

--- sandbox/ericf/gallery-model-sync/user.js
+++ sandbox/ericf/gallery-model-sync/user.js
@@ -1,11 +1,16 @@
 
 var User = Y.Base.create('user', Y.Model, [Y.ModelSync], {
-    root    : '/user/',
-    url     : '{id}/'
+    root    : '/users/',
+    url     : '/user/{id}/'
 }, {
     ATTRS : {
         fullname : {},
         username : {},
         password : {}
     }
+});
+
+var Users = Y.Base.create('users', Y.ModelList, [Y.ModelSync], {
+    model   : User,
+    url     : '/users/'
 });
