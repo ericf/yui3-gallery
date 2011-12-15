0e97702b99950d572153b3b64e08ece529598b27
Eric Ferraiuolo <eferraiuolo@gmail.com>
2011-12-15 17:35:29 -0500

    Doc build.

---
+++ api.js
@@ -0,0 +1,3 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {"classes":["ModelSync.REST","ModelSync.YQL"],"modules":["gallery-model-sync-rest","gallery-model-sync-yql"],"allModules":[{"displayName":"gallery-model-sync-rest","name":"gallery-model-sync-rest","description":"An Extension which provides a RESTful HTTP sync implementation that can be mixed\ninto a Model or ModelList subclass."},{"displayName":"gallery-model-sync-yql","name":"gallery-model-sync-yql","description":"An Extension which provides a YQL sync implementation that can be mixed into a\nModel or ModelList subclass."}]} };
+});
